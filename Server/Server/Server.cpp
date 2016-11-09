d81ff73544229dd522f4bb5f20cff4fbffdb29ae
kimhyeju <hjkim92@kpu.ac.kr>
2016-11-09 12:20:34 +0900

    server클래스 제작
    각 부속 class에서 작동을 받아 클라이언트간의 정보를 넘겨주는 역할을 함

--- Server/Server/Server.cpp
+++ Server/Server/Server.cpp
@@ -45,10 +45,10 @@ void CServer::SendPacket(int id, void *packet)
 	send_over->wsabuf.len = packet_size;
 	unsigned long io_size;
 
-	memcpy(send_over->IOCPbuf, packet, packet_size); 
-	if (true == CPlayer::getInstance().client[id].in_use )
+	memcpy(send_over->IOCPbuf, packet, packet_size);
+	if (true == CPlayer::getInstance().client[id].in_use)
 		WSASend(CPlayer::getInstance().client[id].sock, &send_over->wsabuf, 1,
-			&io_size, NULL, &send_over->overapped, NULL);
+		&io_size, NULL, &send_over->overapped, NULL);
 }
 int CServer::GetNewClient_ID()
 {
@@ -106,26 +106,26 @@ void CServer::ProcessPacket(char* packet, int id){
 	}
 	case CS_ROTATE:{
 
-			m_playerrotate.dir = CPlayer::getInstance().client[id].direction;
-			m_playerrotate.rad = CPlayer::getInstance().client[id].radian;
-			CS_Rotate *rotate = reinterpret_cast<CS_Rotate*>(packet);
-			SC_PlayerRotate playerrotate;
+		m_playerrotate.dir = CPlayer::getInstance().client[id].direction;
+		m_playerrotate.rad = CPlayer::getInstance().client[id].radian;
+		CS_Rotate *rotate = reinterpret_cast<CS_Rotate*>(packet);
+		SC_PlayerRotate playerrotate;
 
-			//EnterCriticalSection(&cs);
-			playerrotate.size = sizeof(SC_PlayerRotate);
-			playerrotate.type = SC_PLAYERROTATE;
-			playerrotate.ID = id;
-			playerrotate.dir = rotate->dir;
-			playerrotate.rad = rotate->rad;
+		//EnterCriticalSection(&cs);
+		playerrotate.size = sizeof(SC_PlayerRotate);
+		playerrotate.type = SC_PLAYERROTATE;
+		playerrotate.ID = id;
+		playerrotate.dir = rotate->dir;
+		playerrotate.rad = rotate->rad;
 
-			CPlayer::getInstance().client[id].direction = playerrotate.dir;
-			CPlayer::getInstance().client[id].radian = playerrotate.rad;
-			//LeaveCriticalSection(&cs);
+		CPlayer::getInstance().client[id].direction = playerrotate.dir;
+		CPlayer::getInstance().client[id].radian = playerrotate.rad;
+		//LeaveCriticalSection(&cs);
 
-			for (int i = 0; i <= usernum; ++i){
-				SendPacket(i, &playerrotate);
-			}	
-			break;
+		for (int i = 0; i <= usernum; ++i){
+			SendPacket(i, &playerrotate);
+		}
+		break;
 	}
 	case CS_READY:{
 		CS_Ready* playerready = reinterpret_cast<CS_Ready*>(packet);
@@ -144,7 +144,7 @@ void CServer::ProcessPacket(char* packet, int id){
 	case CS_ITEMGET:{
 		CS_ItemGet* itemget = reinterpret_cast<CS_ItemGet*>(packet);
 		SC_Item itempacket;
-		
+
 		itempacket = m_cItem->AvoidItem(itemget->itemnum);
 		AddTimer(itemget->itemnum, EVENT_RESP, 3000);
 		//printf("충돌 %d \n", itemget->itemnum);
@@ -155,7 +155,7 @@ void CServer::ProcessPacket(char* packet, int id){
 	case CS_COLLWALL:{
 		CS_CollWall* wallcoll = reinterpret_cast<CS_CollWall*>(packet);
 		SC_PlayerPos wallpos;
-		
+
 		//EnterCriticalSection(&cs);
 		wallpos.ID = id;
 
@@ -206,7 +206,7 @@ void CServer::ProcessPacket(char* packet, int id){
 	}
 	case CS_BOOSTERITEM:{
 		CS_UseBooster* usebooster = reinterpret_cast<CS_UseBooster*>(packet);
-		
+
 		//EnterCriticalSection(&cs);
 		CPlayer::getInstance().client[id].boostertimming = true;
 		CPlayer::getInstance().client[id].boostersort = BOOSTERUSE;
@@ -217,14 +217,14 @@ void CServer::ProcessPacket(char* packet, int id){
 	}case CS_CLIENTANI:{
 		CS_ClientAni* anicl = reinterpret_cast<CS_ClientAni*>(packet);
 		SC_ServerAni aniser;
-		
+
 		//EnterCriticalSection(&cs);
 		aniser.size = sizeof(SC_ServerAni);
 		aniser.type = SC_SERVERANI;
 		aniser.aninum = anicl->aninum;
 		aniser.ID = id;
 		//LeaveCriticalSection(&cs);
-		
+
 		for (int i = 0; i < usernum; ++i)
 			SendPacket(i, &aniser);
 		break;
@@ -261,7 +261,7 @@ void CServer::ProcessPacket(char* packet, int id){
 		if (1 == rank){
 			for (int i = 0; i < usernum; ++i)
 				AddTimer(i, OP_END, 10000);				//game종료 10초 카운트
-														//timer를 for문에?!
+			//timer를 for문에?!
 		}
 		++rank;
 		break;
@@ -276,7 +276,7 @@ void CServer::ProcessPacket(char* packet, int id){
 		break;
 	}
 	}
-	
+
 
 }
 void CServer::ItemRegen(int id){
@@ -447,97 +447,97 @@ void CServer::Accept_thread(){
 	struct sockaddr_in listen_addr;
 	struct sockaddr_in client_addr;
 
-		SOCKET listen_socket = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL,
-			0, WSA_FLAG_OVERLAPPED);
+	SOCKET listen_socket = WSASocket(AF_INET, SOCK_STREAM, IPPROTO_TCP, NULL,
+		0, WSA_FLAG_OVERLAPPED);
 
-		ZeroMemory(&listen_addr, sizeof(listen_addr));
+	ZeroMemory(&listen_addr, sizeof(listen_addr));
 
-		// listen
-		listen_addr.sin_family = AF_INET;
-		listen_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-		listen_addr.sin_port = htons(SERVER_PORT);
-		ZeroMemory(&listen_addr.sin_zero, sizeof(listen_addr.sin_zero));
+	// listen
+	listen_addr.sin_family = AF_INET;
+	listen_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	listen_addr.sin_port = htons(SERVER_PORT);
+	ZeroMemory(&listen_addr.sin_zero, sizeof(listen_addr.sin_zero));
 
-		int ret = ::bind(listen_socket, reinterpret_cast<sockaddr*>(&listen_addr),
-			sizeof(listen_addr));
+	int ret = ::bind(listen_socket, reinterpret_cast<sockaddr*>(&listen_addr),
+		sizeof(listen_addr));
+	if (SOCKET_ERROR == ret){
+		error_display("BIND", WSAGetLastError());
+		exit(-1);
+	}
+	ret = listen(listen_socket, 8);
+	if (SOCKET_ERROR == ret) {
+		error_display("LISTEN", WSAGetLastError());
+		exit(-1);
+	}
+
+	while (true){
+		int addr_size = sizeof(client_addr);
+		SOCKET client_socket = WSAAccept(listen_socket, reinterpret_cast<sockaddr*>(&client_addr),
+			&addr_size, NULL, NULL);
+		if (INVALID_SOCKET == client_socket){
+			error_display("Accept", WSAGetLastError());
+			exit(-1);
+		}
+
+		// 플레이어 접속 / 위치 / 상태
+		int id = GetNewClient_ID();
+		CPlayer::getInstance().client[id].sock = client_socket;
+
+		printf("접속 %s \n ID : %d \n", inet_ntoa(client_addr.sin_addr), id);
+
+		CreateIoCompletionPort(reinterpret_cast<HANDLE>(client_socket),
+			hIOCP, id, 0);
+		unsigned long recv_flag = 0;
+
+		ret = WSARecv(client_socket, &CPlayer::getInstance().client[id].my_overapped.wsabuf, 1, NULL,
+			&recv_flag, &CPlayer::getInstance().client[id].my_overapped.overapped, NULL);
+
 		if (SOCKET_ERROR == ret){
-			error_display("BIND", WSAGetLastError());
-			exit(-1);
-		}
-		ret = listen(listen_socket, 8);
-		if (SOCKET_ERROR == ret) {
-			error_display("LISTEN", WSAGetLastError());
-			exit(-1);
-		}
-
-		while (true){
-			int addr_size = sizeof(client_addr);
-			SOCKET client_socket = WSAAccept(listen_socket, reinterpret_cast<sockaddr*>(&client_addr),
-				&addr_size, NULL, NULL);
-			if (INVALID_SOCKET == client_socket){
-				error_display("Accept", WSAGetLastError());
+			int err_code = WSAGetLastError();
+			if (WSA_IO_PENDING != err_code){
+				error_display("Accept(WSARecv):", err_code);
 				exit(-1);
 			}
-
-			// 플레이어 접속 / 위치 / 상태
-			int id = GetNewClient_ID();
-			CPlayer::getInstance().client[id].sock = client_socket;
-
-			printf("접속 %s \n ID : %d \n", inet_ntoa(client_addr.sin_addr), id);
-
-			CreateIoCompletionPort(reinterpret_cast<HANDLE>(client_socket),
-				hIOCP, id, 0);
-			unsigned long recv_flag = 0;
-
-			ret = WSARecv(client_socket, &CPlayer::getInstance().client[id].my_overapped.wsabuf, 1, NULL,
-				&recv_flag, &CPlayer::getInstance().client[id].my_overapped.overapped, NULL);
-
-			if (SOCKET_ERROR == ret){
-				int err_code = WSAGetLastError();
-				if (WSA_IO_PENDING != err_code){
-					error_display("Accept(WSARecv):", err_code);
-					exit(-1);
-				}
-			}
-			++usernum;					// 접속 시 유저 수
-			// 서버에서 클라로 보내줄 플레이어 패킷
-			// SC_Player
-			SC_InitPlayer initplayer;
-			ZeroMemory(&initplayer, sizeof(initplayer));
-			initplayer = CPlayer::getInstance().PlayerAccept(id);
-			CPlayer::getInstance().client[id].position = initplayer.move;
-			CPlayer::getInstance().client[id].in_use = true;
-			CPlayer::getInstance().client[id].accel = initplayer.accel;
-			CPlayer::getInstance().client[id].speed = initplayer.speed;
-			CPlayer::getInstance().client[id].avoidspeed = 0;
-			CPlayer::getInstance().client[id].presstime = 0;
-			CPlayer::getInstance().client[id].boostertimming = false;
-			CPlayer::getInstance().client[id].boostersort = 0;
-			CPlayer::getInstance().client[id].boostertime = 0;
-			CPlayer::getInstance().client[id].trigger = 0;
-			
-
-			for (int i = 0; i <= usernum; ++i){
-				SendPacket(i, &initplayer);
-			}
-
-			// 이전에 들어와 있는 패킷을 다시 보내주는 작업
-			for (int i = 0; i <= usernum; ++i){
-				if (false == CPlayer::getInstance().client[i].in_use) continue;
-				if (i == id)continue;
-				initplayer.ID = i;
-				initplayer.move = CPlayer::getInstance().client[i].position;
-				SendPacket(id, &initplayer);
-			}
-			///////////////////////////////////////////////////// 유저 수 count다시 짜기
-			// 스테이트 다시짜기
-			//SC_State m_state;
-			m_state = m_cGM->InitGameManager();
-
-			for (int i = 0; i <= usernum; ++i)
-				SendPacket(i, &m_state);
-			
 		}
+		++usernum;					// 접속 시 유저 수
+		// 서버에서 클라로 보내줄 플레이어 패킷
+		// SC_Player
+		SC_InitPlayer initplayer;
+		ZeroMemory(&initplayer, sizeof(initplayer));
+		initplayer = CPlayer::getInstance().PlayerAccept(id);
+		CPlayer::getInstance().client[id].position = initplayer.move;
+		CPlayer::getInstance().client[id].in_use = true;
+		CPlayer::getInstance().client[id].accel = initplayer.accel;
+		CPlayer::getInstance().client[id].speed = initplayer.speed;
+		CPlayer::getInstance().client[id].avoidspeed = 0;
+		CPlayer::getInstance().client[id].presstime = 0;
+		CPlayer::getInstance().client[id].boostertimming = false;
+		CPlayer::getInstance().client[id].boostersort = 0;
+		CPlayer::getInstance().client[id].boostertime = 0;
+		CPlayer::getInstance().client[id].trigger = 0;
+
+
+		for (int i = 0; i <= usernum; ++i){
+			SendPacket(i, &initplayer);
+		}
+
+		// 이전에 들어와 있는 패킷을 다시 보내주는 작업
+		for (int i = 0; i <= usernum; ++i){
+			if (false == CPlayer::getInstance().client[i].in_use) continue;
+			if (i == id)continue;
+			initplayer.ID = i;
+			initplayer.move = CPlayer::getInstance().client[i].position;
+			SendPacket(id, &initplayer);
+		}
+		///////////////////////////////////////////////////// 유저 수 count다시 짜기
+		// 스테이트 다시짜기
+		//SC_State m_state;
+		m_state = m_cGM->InitGameManager();
+
+		for (int i = 0; i <= usernum; ++i)
+			SendPacket(i, &m_state);
+
+	}
 }
 void CServer::Process_Event(event_type nowevent){
 	switch (nowevent.do_event){
@@ -581,15 +581,15 @@ void CServer::Process_Event(event_type nowevent){
 }
 void CServer::CountTime(){								//계속해서 시간을 보내줌
 	//while (PLAYING == m_state.gamestate){
-		SC_Time time;
-		time.size = sizeof(SC_Time);
-		time.type = SC_TIME;
-		time.time = CTimer::GetTime(starttime);
-		
-		//printf("%.2f\n", time.time);
-		for (int i = 0; i < usernum; ++i){
-			SendPacket(i, &time);
-		}
+	SC_Time time;
+	time.size = sizeof(SC_Time);
+	time.type = SC_TIME;
+	time.time = CTimer::GetTime(starttime);
+
+	//printf("%.2f\n", time.time);
+	for (int i = 0; i < usernum; ++i){
+		SendPacket(i, &time);
+	}
 }
 void CServer::SendShoot(int id){
 	SC_Shoot packet;
@@ -616,9 +616,9 @@ void CServer::Worker_thread(){
 		unsigned long io_size;
 		unsigned long key;
 		OVERAPPED_EX *over_ex;
-		GetQueuedCompletionStatus(hIOCP, &io_size, &key, 
+		GetQueuedCompletionStatus(hIOCP, &io_size, &key,
 			reinterpret_cast<LPOVERLAPPED*> (&over_ex), INFINITE);
-	
+
 		// 에러로 인한 접속 종료
 		if (0 == io_size){
 			--usernum;
@@ -666,7 +666,7 @@ void CServer::Worker_thread(){
 				&recv_flag, &over_ex->overapped, NULL);
 		}
 		//SEND
- 		else if (OP_SEND == over_ex->operation_type)
+		else if (OP_SEND == over_ex->operation_type)
 			delete over_ex;
 		else if (OP_RESP == over_ex->operation_type){
 			ItemRegen(key);
